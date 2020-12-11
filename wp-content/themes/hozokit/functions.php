c1c887cb7cedc9a6067c40237c2be58895d00a14
cristiano <cs.almeida@protonmail.ch>
2020-12-11 14:45:45 +0000

    Removed Hozokit functionality away from functions. In order to aid upgrades.

--- wp-content/themes/hozokit/functions.php
+++ wp-content/themes/hozokit/functions.php
@@ -1,72 +1,35 @@
 <?php
-    // Importing Hozokit Internal Plugins
-    require_once(__DIR__ . '/plugins/index.php');
+  /*
+   * Hozokit
+   * The statement below imports Hozokit Core and other internal plugins.
+   * Please keep this require statement at all times at the top.
+   * 
+  */
+  require_once(__DIR__ . '/plugins/index.php');
 
-    // Initializing Timber.
-    require_once(__DIR__ . '/vendor/autoload.php');
-    $timber = new \Timber\Timber();
-    $timber::$dirname = array('templates', 'templates/components/');
+  /*
+   * Functions
+   * Add theme specific functions below.
+   * 
+  */
+  
+  // Insert styles and scripts to be used in the theme.
+  // The stylesheet is compiled from SASS files in /styles, scripts from /scripts using Gulp.
+  function hozokit_load_styles_scripts() {
+    // Retrieves theme version to be provided to bundled styles and script files.
+    $theme_version = wp_get_theme()['Version'];
 
-    class Site extends Timber\Site {
-      public function __construct() {
-        // Enables admin bar styling.
-	      add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
-        add_theme_support( 'post-thumbnails' );
-        add_theme_support( 'menus' );
-        add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
-        add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-        // Makes menu locations available.
-        register_nav_menus(array(
-          'primary' => __('Primary'),
-          'footer' => __('Footer'),
-          // 'other' => __('Other'),
-        ));
-        hozokit_plugins_init();
-        parent::__construct();
-      }
+    // Normalize is used to reset styles. Remove this line if not required or an alternative is being used.
+    wp_enqueue_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.css', null, 'v8.0.0');
 
-      function add_to_context( $context ) {
-        $context['menus'] = array(
-          'primary' => new Timber\Menu('primary'),
-          'footer' => new Timber\Menu('footer'),
-          // 'other' => new Timber\Menu('other'),
-        );
-        $context['site'] = $this;
-        return $context;
-      }
-    }
-    new Site();
-
-    // Insert styles and scripts to be used in the theme.
-    // The stylesheet is compiled from SASS files in /styles, scripts from /scripts using Gulp.
-    function loadStylesAndScripts() {
-      $theme_version = wp_get_theme()['Version'];
-      wp_enqueue_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.css' );
-      wp_enqueue_style( 'style', get_stylesheet_uri() );
-      wp_register_script( 'script', get_template_directory_uri() . '/assets/scripts/bundle.js', "", $theme_version, true );
-      wp_enqueue_script('script');
-      $translation_array = array( 'templateUrl' => get_template_directory_uri() );
-      //after wp_enqueue_script
-      wp_localize_script( 'script', 'wordpress', $translation_array );
-    }
-
-    // Load styles and scripts (might need wp_head or wp_footer).
-    add_action('wp_enqueue_scripts', 'loadStylesAndScripts');
-
-  // Temporary: Resets metabox data positions.
-  // This needs to be commented out when the ACF field structure becomes permanent.
-  function prefix_reset_metabox_positions(){
-    delete_user_meta( wp_get_current_user()->ID, 'meta-box-order_post' );
-    delete_user_meta( wp_get_current_user()->ID, 'meta-box-order_page' );
+    // Enqueues the theme's styles and scripts.
+    wp_enqueue_style( 'style', get_stylesheet_uri(), null,  $theme_version);
+    wp_register_script( 'script', get_template_directory_uri() . '/assets/scripts/bundle.js', "", $theme_version, true );
+    wp_enqueue_script('script');
+    $translation_array = array( 'templateUrl' => get_template_directory_uri() );      
+    //after wp_enqueue_script
+    wp_localize_script( 'script', 'wordpress', $translation_array );
   }
 
-  // Temporary: Remove admin bar in order to avoid extra margins.
-  show_admin_bar(false);
-
-  // Temporary: Keeps Gutenberg from rendering <p> tags on Timber's post.preview.
-  remove_filter( 'the_content', 'wpautop' );
-
-  /* !IMPORTANT: Only run this one when adding new custom post types.
-   * Remove when related features are added.
-  */
-  // flush_rewrite_rules();
+  // Load styles and scripts (might need wp_head or wp_footer).
+  add_action('wp_enqueue_scripts', 'hozokit_load_styles_scripts');
--- wp-content/themes/hozokit/plugins/index.php
+++ wp-content/themes/hozokit/plugins/index.php
@@ -1,10 +1,45 @@
 <?php
-// Initializes all plugins the theme depends on.
-function hozokit_plugins_init() {
   // Used to include Composer dependencies where necessary.
-  define('DIR_VENDOR', __DIR__.'/vendor/');
+  define('DIR_VENDOR', __DIR__ . '/vendor/');
 
-  // Importing Custom Gutenberg Blocks
-  require_once(__DIR__ . '/blocks/index.php');
-  require_once(__DIR__ . '/hozokit-functions/index.php');
-}
+  // Initializes all plugins the theme depends on.
+  function hozokit_plugins_init() {
+    
+    // Importing Custom Gutenberg Blocks
+    require_once(__DIR__ . '/blocks/index.php');
+    require_once(__DIR__ . '/hozokit-core/index.php');
+  }
+
+  // Initializing Timber.
+  require_once(get_template_directory() . '/vendor/autoload.php');
+  $timber = new \Timber\Timber();
+  // This is where Timber will look for components.
+  // That's why the whole directory of a component (e.g `/templates/components/button/index.twig`) does not need to be specified. 
+  $timber::$dirname = array('templates', 'templates/components/');
+
+  class Site extends Timber\Site {
+    public function __construct() {
+      // Enables admin bar styling and adds support for other elements in the theme.
+      add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
+      add_theme_support( 'post-thumbnails' );
+      add_theme_support( 'menus' );
+      add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+      
+      // Adds a few properties to context when the site is initialized.
+      add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+
+      // Initializes internal plugins.
+      hozokit_plugins_init();
+      parent::__construct();
+    }
+
+    // Initializes context with the registered menus and site properties.
+    function add_to_context( $context ) {
+      $context['site'] = $this;
+
+      return $context;
+    }
+  }
+
+  // Instantiates a new site.
+  new Site();
